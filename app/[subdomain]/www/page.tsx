599be2c6e4805bc595191570fdf64574639207cb
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2024-07-11 19:28:53 +0200

    Remove subdomain features

--- app/[subdomain]/www/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from '@chakra-ui/react';
-
-export default function Home() {
-  return (
-    <Text fontSize='8xl' fontWeight='800' fontStyle='italic'>
-      eCom in Motion
-    </Text>
-  );
-}
