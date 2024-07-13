30b1c49db14608a9f980b7bad196e03001a23c0e
Mahmoud Tantouri <26313521+MTraveller@users.noreply.github.com>
2024-07-13 15:00:14 +0200

    Add domain to cookie

--- utils/modeCookie.ts
+++ utils/modeCookie.ts
@@ -6,6 +6,7 @@ export const setModeCookie = async (colorMode: 'dark' | 'light') => {
   cookies().set('colorMode', colorMode === 'dark' ? 'light' : 'dark', {
     secure: true,
     sameSite: 'strict',
+    domain: 'localhost:3000' || 'ecominmotion.com',
   });
 };
 
